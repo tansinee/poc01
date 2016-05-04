3bb75496048f9342e584ccf31b902bf24491408f
Kajornthep Piyanan <Kajornthep.P@kaidee.com>
2016-05-04 17:59:30 +0700

    Add install jupyter script

---
+++ provision/scripts/install_jupyter.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+JUPYTER_PORT=8888
+
+# Disable firewall and allow port
+sudo ufw Disable
+sudo iptables -A INPUT -p tcp --dport $JUPYTER_PORT -j ACCEPT
+
+pip install --pre toree
+sudo /home/vagrant/miniconda/bin/jupyter toree install --user --spark_home=/opt/spark --interpreters=PySpark --python=python
