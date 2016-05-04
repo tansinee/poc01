ad3e389eef1a8c9048e1ce7539e7b38ff81e8507
Jenkins <infra@kaidee.com>
2016-05-04 13:30:32 +0700

    set cassandra config to listen at eth0 instead of localhost

--- provision/scripts/install_cassandra.sh
+++ provision/scripts/install_cassandra.sh
@@ -5,5 +5,9 @@ curl -L http://debian.datastax.com/debian/repo_key | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install -y dsc30 cassandra cassandra-tools
 
+#config cassandra.yaml
+sed -i 's/listen_address: localhost/# listen_address: localhost\nlisten_interface: eth0/' /etc/cassandra/cassandra.yaml
+sed -i 's/rpc_address: localhost/# rpc_address: localhost\nrpc_interface: eth0/' /etc/cassandra/cassandra.yaml
+
 #start cassandra service
 sudo service cassandra start
