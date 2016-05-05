ffd1a3c63be63b5255819b128ab88ca3e4670e2f
Tansinee <tansinee@Betrayer.local>
2016-05-05 12:03:34 +0700

    Modify script to work with spark submit

--- data/ohlc.py
+++ data/ohlc.py
@@ -1,14 +1,27 @@
 from datetime import datetime
-from pyspark.sql.functions import *
 
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SQLContext
+from pyspark.sql import functions as F
+from pyspark.sql.functions import to_date
+
+conf = SparkConf()\
+        .setAppName('batch ohlc') \
+        .setMaster('local')
+sc = SparkContext(conf=conf)
 ctx = SQLContext(sc)
 
-data = ctx.read.format('org.apache.spark.sql.cassandra').options(table='transactions', keyspace='stock', cluster='Test Cluster').load()
+data = ctx.read.format('org.apache.spark.sql.cassandra') \
+            .options(table='transactions', keyspace='stock', cluster='Test Cluster') \
+            .load()
 
-data2 = data.select('symbol', to_date(data.tx_time).alias('batch_time'), 'price')
-min_data = data2.groupBy('symbol', 'batch_time').min()
-min_data.show()
+daily_ohlc = data.select('symbol', to_date(data.tx_time).alias('batch_time'), 'price') \
+                 .groupBy('symbol', 'batch_time') \
+                 .agg(
+                    F.first(data.price).alias('open'),
+                    F.max(data.price).alias('high'),
+                    F.min(data.price).alias('low'),
+                    F.last(data.price).alias('close')
+                 )
 
-
-
-
+daily_ohlc.show()
