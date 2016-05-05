f0ff4e701cd108641075bc28233a016099b13e05
Kajornthep Piyanan <Kajornthep.P@kaidee.com>
2016-05-05 13:02:18 +0700

    Change check jupyter command evaluation

--- provision/scripts/install_jupyter.sh
+++ provision/scripts/install_jupyter.sh
@@ -4,11 +4,13 @@
 sudo ufw Disable
 sudo iptables -A INPUT -p tcp --dport 8888 -j ACCEPT
 
-`which jupyter` || conda install jupyter
+which jupyter || conda install jupyter
 
 cat >> /home/vagrant/.bashrc <<- EOM
+
 # Pyspark Settings
 export PYSPARK_PYTHON=/home/vagrant/miniconda/bin/python
 export PYSPARK_DRIVER_PYTHON="jupyter"
 export PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser --ip 0.0.0.0 --notebook-dir=/vagrant"
+
 EOM
